decf34fcd63d671e5269b3a600d3e50f43c2c594
CTN <info@ctn-originals.com>
2024-01-02 18:14:13 +0100

    Base Setup
    You know the drill, npm install stuff and client read > login stuff

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Client } from 'discord.js';
+
+const client = new Client({
+	intents: [
+		'Guilds',
+		'GuildMessages',
+		'GuildMembers',
+		'MessageContent'
+	]
+});
+
+client.on('ready', (c) => {
+	console.log(`Logged in as ${c.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
