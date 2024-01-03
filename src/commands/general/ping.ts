9fa6cf2b8dbc5c1f28a8d5e747520688f62ca0dc
CTN <info@ctn-originals.com>
2024-01-03 14:19:37 +0100

    Create ping.ts
    first command!

---
+++ src/commands/general/ping.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from "discord.js";
+
+import { cons } from "../../index";
+
+export default {
+	command: {
+		data: new SlashCommandBuilder()
+			.setName("ping")
+			.setDescription("Replies with Pong!"),
+		async execute(interaction: any) {
+			cons.log(`[fg=red]Running[/>] ([fg=cyan]${interaction.commandName}[/>]) [fg=red]command[/>]`);
+			interaction.reply({ content: "Pong!" });
+		},
+	}
+}
