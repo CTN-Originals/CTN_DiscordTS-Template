ceccf82bd9259b6a30693ab9208c96e29b06acf4
CTN <info@ctn-originals.com>
2024-01-07 01:21:57 +0100

    Update ready.ts
    already added testing bullshit

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
-import { ConsoleInstance } from 'better-console-utilities';
 import { Client } from 'discord.js';
+import { ConsoleInstance } from 'better-console-utilities';
 // import ErrorHandler from '../handlers/errorHandler';
 
 const thisCons = new ConsoleInstance();
@@ -8,11 +8,35 @@ export default {
 	name: 'ready',
 	once: true,
 
-	/**
-	 * @param {Client} client The command interaction
-	*/
 	async execute(client: Client) {
 		thisCons.log(`Logged in as ${client.user?.tag}!`);
 		// new ErrorHandler(new Error('test'));
 	},
 };
+
+/* string builder to filter out a 72 char discord token and replace it
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    myString := "Hahaha world [code here] Yes"
+    n := len(myString)
+    last := 0
+    var sb strings.Builder
+    for i := 0; i <= n; i++ {
+        if i >= n || myString[i] == ' ' {
+            if i-last == 72 {
+                sb.WriteString(strings.Repeat("*", i-last))
+            } else {
+                sb.WriteString(myString[last:i])
+            }
+            sb.WriteRune(' ')
+            last = i + 1
+        }
+    }
+
+    fmt.Println(sb.String())
+}
+*/
