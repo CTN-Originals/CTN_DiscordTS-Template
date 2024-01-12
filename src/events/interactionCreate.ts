a3c50efce246ee00b3c01bdefa80989e802a425c
CTN <info@ctn-originals.com>
2024-01-12 23:51:55 +0100

    error webhook log
    on errors, a webhook (id + token en .env) logs the error with the content in an embed nicely displayed.

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,6 @@
 import { 
-	CommandInteraction, 
+	BaseInteraction,
+	CommandInteraction,
 	Client,
 	InteractionType,
 	ComponentType,
@@ -11,6 +12,7 @@ import { ConsoleInstance } from 'better-console-utilities';
 
 import { EmitError, eventConsole } from '.';
 import { errorConsole } from '..';
+import { IInteractionTypeData, getInteractionType } from '../utils/interactionUtils';
 import generalData from '../data/generalData';
 import { ErrorObject } from '../handlers/errorHandler';
 
@@ -21,30 +23,24 @@ export default {
 	once: false,
 
     /** @param {CommandInteraction} interaction The command interaction */
-	async execute(interaction) { //Todo: type this properly
-		// if (!interaction.isChatInputCommand()) return;
-        const client = interaction.client;
-
-		if (interaction.isCommand() || interaction.isButton() || interaction.isStringSelectMenu()) {
-			const nameKey = interaction.isCommand() ? 'commandName' : interaction.isButton() ? 'customId' : 'customId';
-			if (client.commands.has(interaction[nameKey])) {
-				await this.executeInteraction(interaction, nameKey);
-			}
+	async execute(interaction: BaseInteraction) {
+		const interactionType = getInteractionType(interaction);
+		
+		if (!interactionType.commandKey || interactionType.type == InteractionType.Ping) {
+			thisConsole.log(`No Command Key || PING Interaction Received`);
+			thisConsole.logDefault(interaction);
+			return;
 		}
+		await this.executeInteraction(interaction, interactionType.commandKey);
 	},
 
-	/** 
-	 * @param {CommandInteraction} interaction The command interaction
-	 * @param {Client} client The client
-	 * @param {string} nameKey The key to get the command name from the interaction
-	*/
-	async executeInteraction(interaction, nameKey: string) {
+	async executeInteraction(interaction: BaseInteraction, nameKey: string) {
 		let response: any = null;
 		try { 
 			const command = interaction.client.commands.get(interaction[nameKey]);
 			response = await command.execute(interaction);
 		} catch (err) {
-			const errorObject: ErrorObject = await EmitError(err as Error); //?? does "as Error" fuck this? ever??
+			const errorObject: ErrorObject = await EmitError(err as Error, interaction); //?? does "as Error" fuck this? ever??
 
 			let content = `There was an error while executing this interaction`
 			if (generalData.development) {
@@ -55,19 +51,12 @@ export default {
 				content: content,
 				ephemeral: true,
 			};
-
-			if (!interaction.replied) { //?? does this need "&& !interaction.deferred"?
-				await interaction.reply(replyContent).catch(EmitError);
-			}
-			else if (interaction instanceof CommandInteraction) {
-				await interaction.followUp(replyContent).catch(EmitError);
-				
-			}
-
-			//TODO: Add this feature to the errorHandler instead
-			const logChannel: TextChannel = interaction.client.channels.cache.get(process.env.LOG_CHANNEL_ID as string);
-			if (logChannel) {
-				logChannel.send(replyContent.content);
+			
+			if (interaction.isRepliable()) {
+				if (!interaction.replied) { //?? does this need "&& !interaction.deferred"?
+					await interaction.reply(replyContent).catch(EmitError);
+				}
+				else await interaction.followUp(replyContent).catch(EmitError);
 			}
 
 			response = err;
@@ -78,35 +67,14 @@ export default {
 
 	outputLog(interaction, response = null) {
 		if (generalData.logging.interaction.enabled) {
-			// console.log(interaction);
-			const commandName = interaction.type == InteractionType.MessageComponent ? 
-				interaction.customId : 
-				interaction.commandName
-			;
-			let interactionType = '';
-			switch(interaction.type) {
-				case InteractionType.ApplicationCommand: { interactionType = 'Command'; } break;
-				case InteractionType.ModalSubmit: { interactionType = 'Modal Submit'; } break;
-				case InteractionType.MessageComponent: { 
-					switch(interaction.componentType) {
-						case ComponentType.ActionRow: { interactionType = 'ActionRow'; } break;
-						case ComponentType.Button: { interactionType = 'Button'; } break;
-						case ComponentType.StringSelect: { interactionType = 'Select(String)'; } break;
-						case ComponentType.ChannelSelect: { interactionType = 'Select(Channel)'; } break;
-						case ComponentType.MentionableSelect: { interactionType = 'Select(Mentionable)'; } break;
-						case ComponentType.RoleSelect: { interactionType = 'Select(Role)'; } break;
-						case ComponentType.TextInput: { interactionType = 'TextInput'; } break;
-						case ComponentType.UserSelect: { interactionType = 'Select(User)'; } break;
-						default: break;
-					}
-				} break;
-				default: { interactionType = 'Unknown'; } break;
-			}
+			console.log(interaction);
+			const interactionType: IInteractionTypeData = getInteractionType(interaction)
+			const commandName = (interactionType.commandKey) ? interaction[interactionType.commandKey] : 'undefined'
 
 			var logMessage = [
-				`\n[fg=cyan]${interaction.user.username}[/>]([fg=cc00ff]${interaction.user.id}[/>])`,
+				`\n[fg=cyan]${interaction.user.username}[/>] ([fg=aaaaaa]${interaction.user.id}[/>])`,
 				`in [fg=black bg=white]#${interaction.channel.name}[/>]`,
-				`\n[fg=0080ff]${interactionType}[/>]: [fg=ffaa00]/[/>][fg=00cc00]${commandName}[/>]`,
+				`\n[fg=0080ff]${interactionType.display}[/>]: [fg=888888]/[/>][fg=00cc00]${commandName}[/>]`,
 			].join(' ');
 			
 
