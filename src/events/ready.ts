07a594df56697733411f4780e3d921849ab2a2ea
CTN <info@ctn-originals.com>
2024-01-19 13:11:24 +0100

    Update ready.ts
    Added a face interaction object for command testing.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,9 @@
-import { Client, Events } from 'discord.js';
+import 'dotenv/config';
+import { APIUser, Client, CommandInteraction, EmbedBuilder, Events, Guild, Message, TextChannel, User } from 'discord.js';
 
 import { ConsoleInstance } from 'better-console-utilities';
+import { testEmbed, validateEmbed } from '../utils/embedUtils';
+import { testWebhook } from '..';
 
 // import ErrorHandler from '../handlers/errorHandler';
 
@@ -11,10 +14,50 @@ export default {
 	once: true,
 
 	async execute(client: Client) {
-		thisCons.log(`Logged in as ${client.user?.tag}!`);
-		// thisCons.logDefault(client);
-		client.emit(Events.Error, new Error("I'm an error"));
+		thisCons.log(`Logged in as ${client.user?.tag}!\n`);
+		thisCons.logDefault(client);
+
+		this.runTests(client);
 	},
+
+	async runTests(client: Client) {
+		const guild = client.guilds.cache.get(process.env.DEV_GUILD_ID!);
+		const fakeInteraction: any = {
+			client: client,
+			guild: guild as Guild,
+			commandName: 'ping',
+			type: 2,
+			componentType: 1,
+			guildId: guild?.id,
+			channelId: process.env.DEV_TEST_CHANNEL_ID,
+			user: {
+				id: process.env.DEV_TEST_USER_ID,
+				username: 'keybotkiller',
+				_equals: (user) => {return true},
+			},
+			channel: {
+				id: process.env.DEV_TEST_CHANNEL_ID,
+				name: 'bot-testing',
+			} as Partial<TextChannel>,
+			options: {
+				data: {}
+			},
+			isRepliable: () => {return true},
+			reply: async (replyContent: string | {content: string, ephemeral: boolean, embeds: EmbedBuilder[], components: any[]}): Promise<Message|boolean> => {
+				const channel = guild?.channels.cache.get(fakeInteraction.channel.id);
+				if (!channel || !(channel instanceof TextChannel)) return false;
+				return channel.send(replyContent);
+			},
+		}
+
+		client.emit(Events.InteractionCreate, fakeInteraction);
+
+		// const validatedTestEmbed = validateEmbed(testEmbed);
+		// console.log(validatedTestEmbed);
+		// testWebhook.send({
+		// 	embeds: [validatedTestEmbed]
+		// });
+	}
 };
 
 /* string builder to filter out a 72 char discord token and replace it
@@ -43,3 +86,5 @@ func main() {
     fmt.Println(sb.String())
 }
 */
+
+// _equals(user) {`\n    return (\n      user &&\n      this.id === user.id &&\n      this.username === user.username &&\n      this.discriminator === user.discriminator &&\n      this.globalName === user.global_name &&\n      this.avatar === user.avatar &&\n      this.flags?.bitfield === user.public_flags &&\n      ('banner' in user ? this.banner === user.banner : true) &&\n      ('accent_color' in user ? this.accentColor === user.accent_color : true)\n    );\n  `}
