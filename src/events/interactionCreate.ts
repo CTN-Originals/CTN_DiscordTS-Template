fb5e7ae8af9d435d9949209a4f3ff7826505d806
CTN <info@ctn-originals.com>
2025-03-08 13:03:39 +0100

    events/** merge

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,25 +1,75 @@
-import { 
-	CommandInteraction,
-	InteractionType,
-	Events,
-	CommandInteractionOption,
-	ChatInputCommandInteraction,
-	Interaction,
-	MessageContextMenuCommandInteraction,
-	UserContextMenuCommandInteraction,
-	ButtonInteraction,
+ 
+import {
 	AnySelectMenuInteraction,
+	APIApplicationCommandGuildInteraction,
+	BaseInteraction,
+	ChatInputCommandInteraction,
+	CommandInteraction,
+	CommandInteractionOption,
+	ContextMenuCommandInteraction,
+	EmbedBuilder,
+	Events,
+	Interaction,
+	InteractionReplyOptions,
+	InteractionType,
+	MessageFlags,
+	PermissionsBitField
 } from 'discord.js';
 
 import { ConsoleInstance } from 'better-console-utilities';
 
 import { EmitError } from '.';
-import { IInteractionTypeData, getHoistedOptions, getInteractionType } from '../utils/interactionUtils';
+import { client } from '..';
 import { ColorTheme, GeneralData } from '../data';
-import { errorConsole, ErrorObject } from '../handlers/errorHandler';
-import { IAnyInteractionField, IButtonCollectionField, ICommandField, IContextMenuField, ISelectMenuCollectionField } from '../handlers/commandBuilder/data';
+import type {
+	BaseButtonCollection,
+	BaseEmbedCollection,
+	BaseMethodCollection,
+	BaseSelectMenuCollection,
+	CommandInteractionData,
+	IButtonCollectionField,
+	ICommandObjectContent,
+	IContextMenuObjectContent,
+	ISelectMenuCollectionField,
+	TLogEnvironment,
+	TLogLevel
+} from '../handlers/commandBuilder';
+import {
+	LOG_ENVIRONMENT,
+	LOG_LEVEL
+} from '../handlers/commandBuilder';
+import { ComponentValueStorage } from '../handlers/componentValueStorage';
+import type { ErrorObject } from '../handlers/errorHandler';
+import { errorConsole } from '../handlers/errorHandler';
+import { hexToBit, removeDuplicates } from '../utils';
+import { validateEmbed } from '../utils/embedUtils';
+import type { IInteractionTypeData } from '../utils/interactionUtils';
+import { getHoistedOptions, getInteractionType } from '../utils/interactionUtils';
 
 const thisConsole = new ConsoleInstance();
+
+function lackingPermissionEmbed(interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction, neededPerms: PermissionsBitField): EmbedBuilder {
+	const selfMember = interaction.guild!.members.me!;
+	const missingRole: string[] = selfMember.permissions.missing(neededPerms);
+	const missingChannel: string[] = [];
+	
+	if (interaction.channel?.isTextBased() && !interaction.channel.isDMBased()) {
+		missingChannel.push(...selfMember.permissionsIn(interaction.channel).missing(neededPerms));
+	}
+
+	const missingPerms = removeDuplicates([...missingRole, ...missingChannel]);
+	
+	const embed = new EmbedBuilder({
+		title:       'Lacking Permission',
+		description: [
+			'To be able to perform this action, I need the following permission(s):',
+			`**${missingPerms.join('**\n**')}**`,
+		].join('\n'),
+		color: hexToBit(ColorTheme.embeds.notice)
+	});
+
+	return embed;
+}
 
 export default {
 	name: Events.InteractionCreate,
@@ -29,7 +79,7 @@ export default {
 		const interactionType = getInteractionType(interaction);
 		
 		if (!interactionType.commandKey || interactionType.type == InteractionType.Ping) {
-			errorConsole.log(`No Command Key || PING Interaction Received`);
+			errorConsole.log('No Command Key || PING Interaction Received');
 			thisConsole.logDefault(interaction);
 			return;
 		}
@@ -37,37 +87,95 @@ export default {
 	},
 
 	async executeInteraction(interaction: Interaction, nameKey: string) {
-		let response: any = null;
+		let response: null | string | boolean = null;
 
+		const getInteractionData = (): CommandInteractionData<BaseButtonCollection, BaseSelectMenuCollection, BaseEmbedCollection, BaseMethodCollection> | IButtonCollectionField | ISelectMenuCollectionField | null | undefined => {
+			if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+				return interaction.client.commands.get(interaction[nameKey]);
+			}
+			else if (interaction.isButton() || interaction.isAnySelectMenu()) {
+				const componentType = (interaction.isButton()) ? 'button' : 'selectMenu';
+				const parent = client[componentType + 's'].get(interaction.customId);
+				if (!parent) {
+					throw new Error(`Unknown component interaction: "${interaction.customId}"`);
+				}
+				
+				const command = client.commands.get(parent);
+				if (!command) {
+					throw new Error(`Component origin unknown: "${parent}" > "${interaction.customId}"`);
+				}
+
+				const componentCollection: BaseButtonCollection | BaseSelectMenuCollection = command.collection[componentType + 's'];
+
+				for (const key in componentCollection) {
+					const component: IButtonCollectionField | ISelectMenuCollectionField = componentCollection[key];
+					if (component.content.customId === interaction.customId) {
+						return component;
+					}
+				}
+			}
+
+			return null;
+		};
+
+		let commandErrored = false;
+		let interactionData: ICommandObjectContent | IContextMenuObjectContent | IButtonCollectionField | ISelectMenuCollectionField | undefined;
+		let interactionObject: CommandInteractionData<BaseButtonCollection, BaseSelectMenuCollection, BaseEmbedCollection, BaseMethodCollection> | IButtonCollectionField | ISelectMenuCollectionField | null | undefined;
+		
 		try {
-			let data: IAnyInteractionField | undefined;
-			if (interaction.isChatInputCommand()) {
-				data = interaction.client.commands.get(interaction[nameKey]);
-				response = await (data as ICommandField).execute(interaction as ChatInputCommandInteraction);
+			interactionObject = getInteractionData();
+
+			if (!interactionObject) {
+				throw new Error(`Unknown interaction: "${interaction[nameKey]}"`);
 			}
-			else if (interaction.isContextMenuCommand()) {
-				data = interaction.client.contextMenus.get(interaction[nameKey]);
-				response = await (data as IContextMenuField).execute(interaction as MessageContextMenuCommandInteraction | UserContextMenuCommandInteraction);
+
+			if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+				interactionData = (interactionObject as CommandInteractionData<BaseButtonCollection, BaseSelectMenuCollection, BaseEmbedCollection, BaseMethodCollection>).command;
+				
+				if (interaction.inGuild()) {
+					const data = interactionData.data;
+					const perms = data.requiredPermissionBitField;
+					const selfMember = interaction.guild!.members.me;
+
+					if (!selfMember?.permissions.has(perms)) {
+						await interaction.reply({
+							content: 'I am lacking the required permission(s) to perform this action.',
+							embeds:  [validateEmbed(lackingPermissionEmbed(interaction, perms))],
+							flags:   [((!GeneralData.development) ? MessageFlags.Ephemeral : MessageFlags.SuppressNotifications)]
+						});
+
+						response = 'Lacking the required permission(s) to perform this action';
+					}
+				}
 			}
-			else if (interaction.isButton()) {
-				data = interaction.client.buttons.get(interaction[nameKey]);
-				response = await (data as IButtonCollectionField).execute(interaction as ButtonInteraction);
+			else {
+				interactionData = (interactionObject as IButtonCollectionField | ISelectMenuCollectionField);
+
+				if (!interaction.isAutocomplete() && !interaction.isModalSubmit()) {
+					if (!ComponentValueStorage.storageIncludesMessage(interaction.message.id)) {
+						ComponentValueStorage.registerMessage(interaction.message.id, interaction);
+					}
+					if (interaction.isAnySelectMenu()) {
+						ComponentValueStorage.setValue(interaction.message.id, interaction.customId, interaction.values);
+					}
+				}
 			}
-			else if (interaction.isAnySelectMenu()) {
-				data = interaction.client.buttons.get(interaction[nameKey]);
-				response = await (data as unknown as ISelectMenuCollectionField).execute(interaction as AnySelectMenuInteraction);
+
+			if (response === null) {
+				response = await interactionData.execute(interaction as never);
 			}
 		} catch (err) {
-			const errorObject: ErrorObject = await EmitError(err as Error, interaction);
+			commandErrored = true;
+			const errorObject: ErrorObject = await EmitError((err instanceof Error) ? err : new Error(err as string), interaction);
 
-			let content = `There was an error while executing this interaction`
+			let content = 'There was an error while executing this interaction';
 			if (GeneralData.development) {
 				content += '\n```ts\n' + errorObject.formatError({shortenPaths: true, colorize: false}) + '\n```';
 			}
 
-			const replyContent = {
+			const replyContent: InteractionReplyOptions = {
 				content: content,
-				ephemeral: true,
+				flags:   [((!GeneralData.development) ? MessageFlags.Ephemeral : MessageFlags.SuppressNotifications)],
 			};
 			
 			if (interaction.isRepliable()) {
@@ -77,70 +185,99 @@ export default {
 				else await interaction.followUp(replyContent).catch(EmitError);
 			}
 
-			response = err;
+			response = err as string;
 		}
 
-		this.outputLog(interaction, response);
+		const logLevel = 
+			(interactionObject && interactionObject.logLevel !== undefined) ? 
+			  interactionObject.logLevel : LOG_LEVEL.ALWAYS;
+
+		const logEnvironment = 
+			(interactionObject && interactionObject.logEnvironment !== undefined) ? 
+			  interactionObject.logEnvironment : LOG_ENVIRONMENT.ALL;
+
+		const commandState: TLogLevel = (commandErrored === true) ? LOG_LEVEL.ERROR : (response === true) ? LOG_LEVEL.SUCCESS : LOG_LEVEL.FAIL;
+		const commandEnvironment: TLogEnvironment = (GeneralData.production) ? LOG_ENVIRONMENT.PRODUCTION : (GeneralData.beta) ? LOG_ENVIRONMENT.BETA : LOG_ENVIRONMENT.DEVELOPMENT; 
+
+		const logLevelState = (
+			logLevel !== LOG_LEVEL.NEVER &&
+			logLevel === LOG_LEVEL.ALWAYS ||
+			(logLevel & commandState) === commandState
+		);
+		const logEnvironmentState = (
+			logEnvironment === LOG_ENVIRONMENT.ALL ||
+			(logEnvironment & commandEnvironment) === commandEnvironment
+		);
+
+		if (GeneralData.logging.interaction.enabled && logLevelState && logEnvironmentState) {
+			this.outputLog(interaction, response as string);
+		}
 	},
 
-	outputLog(interaction, response = null) {
-		if (GeneralData.logging.interaction.enabled) {
-			const interactionType: IInteractionTypeData = getInteractionType(interaction)
-			const logFields = {
-				commandName: '',
-				subCommand: '',
-				subCommandGroup: '',
-				commandOptions: '',
-				commandValues: '',
-				commandType: interactionType.display,
-				channelName: interaction.channel.name,
-				channelId: interaction.channelId,
-				userId: interaction.user.id,
-				userName: interaction.user.username,
-				response: (response) ? response : null,
-			};
+	outputLog(interaction: BaseInteraction, response: string | null = null) {
+		const interactionType: IInteractionTypeData = getInteractionType(interaction);
+		const logFields = {
+			commandName:     '',
+			subCommand:      '',
+			subCommandGroup: '',
+			commandOptions:  '',
+			commandValues:   '',
+			commandType:     interactionType.display,
+			channelName:     (interaction as unknown as APIApplicationCommandGuildInteraction).channel?.name,
+			channelId:       interaction.channelId,
+			userId:          interaction.user.id,
+			userName:        interaction.user.username,
+			response:        (response) ? response : null,
+		};
 
-			if (interaction[interactionType.commandKey!] !== undefined) {
-				logFields.commandName = interaction[interactionType.commandKey!] as string;
-				
-				if (interaction instanceof ChatInputCommandInteraction) {
-					const subCommandGroup = interaction.options.getSubcommandGroup(false);
-					const subCommand = interaction.options.getSubcommand(false);
-					logFields.subCommandGroup += (subCommandGroup) ? subCommandGroup : '';
-					logFields.subCommand += (subCommand) ? subCommand : '';
-				}
-			}
+		if (interaction[interactionType.commandKey!] !== undefined) {
+			logFields.commandName = interaction[interactionType.commandKey!] as string;
 			
-			if (interaction.options?.data && interaction.options.data.length > 0) {
-				const hoistedOptions = getHoistedOptions((interaction as CommandInteraction).options.data as CommandInteractionOption[]);
-				logFields.commandOptions = hoistedOptions.map(option => `[fg=${ColorTheme.colors.orange.asHex}]${option.name}[/>]:${option.value}`).join(' [st=dim,bold]|[/>] ');
+			if (interaction instanceof ChatInputCommandInteraction) {
+				const subCommandGroup = interaction.options.getSubcommandGroup(false);
+				const subCommand = interaction.options.getSubcommand(false);
+				logFields.subCommandGroup += (subCommandGroup) ? subCommandGroup : '';
+				logFields.subCommand += (subCommand) ? subCommand : '';
 			}
-			if (interaction.values && interaction.values.length > 0) {
-				logFields.commandValues = `[ ${interaction.values.join('[st=dim,bold], [/>]')} ]`
-			}
+		}
+		
+		if ((interaction as ChatInputCommandInteraction).options?.data && (interaction as ChatInputCommandInteraction).options.data.length > 0) {
+			const hoistedOptions = getHoistedOptions((interaction as CommandInteraction).options.data as CommandInteractionOption[]);
+			logFields.commandOptions = hoistedOptions.map(option => `[fg=${ColorTheme.colors.orange.asHex}]${option.name}[/>]:${option.value}`).join(' [st=dim,bold]|[/>] ');
+		}
+		if ((interaction as AnySelectMenuInteraction).values && (interaction as AnySelectMenuInteraction).values.length > 0) {
+			logFields.commandValues = `[ ${(interaction as AnySelectMenuInteraction).values.join('[st=dim,bold], [/>]')} ]`;
+		}
 
 
-			const logMessage: string[] = [];
-			logMessage.push([
-				`[fg=${ColorTheme.colors.blue.asHex}]${logFields.commandType}[/>]: [fg=${ColorTheme.colors.green.asHex} st=bold]${logFields.commandName}[/>]`,
-				`${(logFields.subCommandGroup) ? `[st=dim]>[/>] [fg=${ColorTheme.colors.green.asHex}]${logFields.subCommandGroup}[/>]` : ''}`,
-				`${(logFields.subCommand) ? `[st=dim]>[/>] [fg=${ColorTheme.colors.green.asHex}]${logFields.subCommand}[/>]` : ''}`
-			].join(' '));
+		const logMessage: string[] = [];
+		logMessage.push([
+			`[fg=${ColorTheme.colors.blue.asHex}]${logFields.commandType}[/>]: [fg=${ColorTheme.colors.green.asHex} st=bold]${logFields.commandName}[/>]`,
+			`${(logFields.subCommandGroup) ? `[st=dim]>[/>] [fg=${ColorTheme.colors.green.asHex}]${logFields.subCommandGroup}[/>]` : ''}`,
+			`${(logFields.subCommand) ? `[st=dim]>[/>] [fg=${ColorTheme.colors.green.asHex}]${logFields.subCommand}[/>]` : ''}`
+		].join(' '));
 
-			if (logFields.commandOptions) logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]options[/>]: ${logFields.commandOptions}`);
-			if (logFields.commandValues) logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]values[/>]: ${logFields.commandValues}`);
+		if (logFields.commandOptions) logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]options[/>]: ${logFields.commandOptions}`);
+		if (logFields.commandValues) logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]values[/>]: ${logFields.commandValues}`);
 
+		logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]user[/>]: [fg=${ColorTheme.colors.cyan.asHex}]${logFields.userName}[/>] (${logFields.userId})`);
+		if (interaction.guild) {
 			logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]guild[/>]: [fg=${ColorTheme.colors.yellow.asHex}]${interaction.guild.name}[/>] (${interaction.guild.id})`);
-			logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]user[/>]: [fg=${ColorTheme.colors.cyan.asHex}]${logFields.userName}[/>] (${logFields.userId})`);
 			logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]channel[/>]: [fg=${ColorTheme.colors.purple.asHex}]${logFields.channelName}[/>] (${logFields.channelId})`);
+		} else {
+			logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]guild[/>]: [fg=${ColorTheme.colors.yellow.asHex}]USER_DM[/>]`);
+		}
 
-			if (logFields.response !== '') {
-				logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]Response[/>]:`);
+		if (logFields.response !== '') {
+			logMessage.push(`[fg=${ColorTheme.colors.blue.asHex}]Response[/>]:`);
+			if (typeof response === 'object') {
 				thisConsole.log('\n' + logMessage.join('\n'), response);
+			} else {
+				thisConsole.log('\n' + logMessage.join('\n') + ' ' + response);
 			}
-			else {
-				thisConsole.log('\n' + logMessage.join('\n') + '\n');
-			}
+		}
+		else {
+			thisConsole.log('\n' + logMessage.join('\n') + '\n');
 		}
 	}
 };
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,39 +1,255 @@
-import 'dotenv/config';
-import { Client, Collection, Events, GuildMember, Routes } from 'discord.js';
+import type { Client, ComponentType, EmbedBuilder, Guild, Interaction, Message } from 'discord.js'
+import { Events, InteractionType, Routes, TextChannel } from 'discord.js'
+import 'dotenv/config'
 
-import { ConsoleInstance } from 'better-console-utilities';
+import { ConsoleInstance } from 'better-console-utilities'
 
-import { GeneralData } from '../data';
-import { DevEnvironment } from '../data';
+import { DevEnvironment, GeneralData } from '../data'
+import { Mentionable } from '../data/orm/mentionables'
 
-const thisConsole = new ConsoleInstance();
+import { EmitError } from '.'
+
+
+// import ErrorHandler from '../handlers/errorHandler';
+
+const thisConsole = new ConsoleInstance()
 
 export default {
 	name: Events.ClientReady,
 	once: true,
 
 	async execute(client: Client, ...args: any[]) {
-		thisConsole.log(`Logged in as ${client.user?.tag}!\n`);
-		const devGuildMembers = await client.guilds.cache.get(process.env.DEV_GUILD_ID!)?.members.fetch();
+		thisConsole.log(`Logged in as ${client.user?.tag}!\n`)
 
 		if (GeneralData.development) {
-			DevEnvironment.client = client;
-			DevEnvironment.memberList = devGuildMembers as Collection<string, GuildMember>;
+			DevEnvironment.client = client
+			// devEnvironment.memberList = devGuildMembers as Collection<string, GuildMember>;
 
-			DevEnvironment.guild = client.guilds.cache.get(process.env.DEV_GUILD_ID!);
-			DevEnvironment.user = await client.users.fetch(process.env.DEV_TEST_USER_ID!);
-			DevEnvironment.member = DevEnvironment.memberList.get(process.env.DEV_TEST_USER_ID!);
-			DevEnvironment.channel = DevEnvironment.guild?.channels.cache.get(process.env.DEV_TEST_CHANNEL_ID!);
+			DevEnvironment.guild = client.guilds.cache.get(process.env.DEV_GUILD_ID!)
+			DevEnvironment.user = await client.users.fetch(process.env.DEV_TEST_USER_ID!)
+			DevEnvironment.member = DevEnvironment.memberList.get(process.env.DEV_TEST_USER_ID!)
+			DevEnvironment.channel = DevEnvironment.guild?.channels.cache.get(process.env.DEV_TEST_CHANNEL_ID!) as TextChannel
 
-			DevEnvironment.restCommands = await client.rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.DEV_GUILD_ID!)) as {id: string, name: string, type: number, guild_id: string}[];
+			DevEnvironment.restCommands = await client.rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.DEV_GUILD_ID!)) as {id: string, name: string, type: number, guild_id: string}[]
 
 			// thisConsole.logDefault('Dev Environment:', DevEnvironment);
 		}
 
-		this.runTests(client);
+		this.Initialize(client)
+
+		if (GeneralData.development) {
+			this.runTests(client)
+		}
+	},
+
+	async Initialize(client: Client) {
+		client.guilds.cache.forEach(async guild => {
+			Mentionable.initialize(guild)
+		})
 	},
 
 	async runTests(client: Client) {
-		// const guild: Guild|undefined = client.guilds.cache.get(process.env.DEV_GUILD_ID!);
+		const guild: Guild = client.guilds.cache.get(process.env.DEV_GUILD_ID!)!
+		const channel: TextChannel = await DevEnvironment.client?.channels.fetch(DevEnvironment.channelId) as TextChannel
+
+		// const role = await guild.roles.cache.get('1309653896788050043')!;
+		// const mentionable = await Mentionable.get(guild.id, role.id)!;
+
+		// const channelSelect = RoleCooldownCommand.selectMenus.buildOne<ChannelSelectMenuBuilder>(RoleCooldownCommand.selectMenus.channelSelection.content);
+		// const row: any = new ActionRowBuilder().addComponents(channelSelect);
+
+		// const msg = await channel.send({
+		// 	embeds: RoleCooldownCommand.embeds.mentionableInfo(mentionable!, role),
+		// 	components: [row]
+		// });
+
+		// testWebhook.send({
+		// 	embeds: [await ListCommand.embeds.getCurrentCooldownsEmbed(guild, 'all')]
+		// })
+
+		// console.log(getInteractionObject(PingCommand.command));
+		// console.log(getInteractionObject(PingCommand.buttons.butt));
+		// console.log(getInteractionObject(PingCommand.selectMenus.str));
+
+		// const commands = await client.application?.commands.fetch({force: true, cache: true, guildId: DevEnvironment.guildId});
+		// const ping = commands?.get('1310295745215336482')!;
+		// console.log(ping);
+		// console.log(await guild.commands.permissions.fetch({}))
+		// console.log(await ping.permissions.fetch({guild: guild}))
+
+		
+		// const mentionTest = new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'edit',
+		// 	options:    [
+		// 		{name: 'role', value: {id: '1309653896788050043', name: '!q', hexColor: '#000000'}},
+		// 		// {name: 'global-cooldown', value: '0'},
+		// 		{name: 'channel-cooldown', value: '6127s'},
+		// 		{name: 'user-cooldown', value: '24d 23h 5m 32s'},
+		// 	]
+		// })
+		// await mentionTest.execute()
+
+		// channel.send({
+		// 	content: '``` ```',
+		// 	embeds: [RoleCooldownCommand.embeds.scopeSettingInstructions('channel'), RoleCooldownCommand.embeds.scopeSettingInstructions('role')]
+		// });
+
+		// await new Promise<void>((resolve) => setTimeout(() => {resolve()}, 2000));
+		// mentionTest.execute();
+
+		
+		//#region cooldown input validation
+		// new FakeInteraction('test').execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '1203'}
+		// 	]
+		// }).execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '123minutes456sec'}
+		// 	]
+		// }).execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '12s-1m'}
+		// 	]
+		// }).execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '0d 15m 0:00'}
+		// 	]
+		// }).execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '23grm 43rts'}
+		// 	]
+		// }).execute();
+		// new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'add',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'},
+		// 		{name: 'cooldown', value: '123gr 456te'}
+		// 	]
+		// }).execute();
+
+		// const removeRole = new FakeInteraction('rolecooldown', {
+		// 	subCommand: 'remove',
+		// 	options: [
+		// 		{name: 'role', value: '1309653896788050043'}
+		// 	]
+		// })
+		//#endregion
+
+
+		// try {
+		// 	const message = await DevEnvironment.channel?.messages.fetch().then(list => list.find(m => m.id === '1309304556127260732'))
+		// 	await message?.edit({embeds: [await getCurrentCooldownsEmbed(DevEnvironment.guild!, 'all')]})
+		// } catch (e) {
+		// 	EmitError(e as Error)
+		// }
 	}
-};
+}
+
+type InteractionOptionEntry = {name: string, value: any}
+type FakeInteractionInput = {
+	subCommand?: string,
+	options: InteractionOptionEntry[]
+}
+
+class FakeInteractionOptions {
+	constructor(
+		public _hoistedOptions: InteractionOptionEntry[] = [],
+		public subCommand?: string,
+		public group?: string
+	) {}
+
+	public get(option: string) {
+		return this._hoistedOptions.find(o => o.name === option)
+	}
+	public getString(option: string) { return this.get(option)?.value }
+	public getRole(option: string) { return this.get(option)?.value }
+	//TODO add typed getters like getString or getRole...
+
+	public getSubcommand(required: boolean) { return this.subCommand }
+	public getSubcommandGroup(required: boolean) { return this.group }
+
+	public get _subcommand() { return this.subCommand }
+
+	public get data() { //TODO include groups
+		if (!this.subCommand) { return this._hoistedOptions }
+		else {
+			return [{
+				name:    this.subCommand,
+				options: this._hoistedOptions,
+				type:    1
+			}]
+		}
+	}
+}
+
+class FakeInteraction {
+	public client: Client<boolean> = DevEnvironment.client!
+	public guild: Guild = DevEnvironment.guild!
+	public type: InteractionType = InteractionType.ApplicationCommand
+	public componentType: ComponentType = 1
+
+	public user: TODO
+	public channel: TODO
+
+	public options: FakeInteractionOptions
+
+	constructor(
+		public commandName: string,
+		options?: FakeInteractionInput
+	) {
+		this.user = {
+			id:       process.env.DEV_TEST_USER_ID!,
+			username: 'TEST_USER',
+			_equals:  (user) => {return true},
+		}
+		this.channel = DevEnvironment.channel
+
+		this.options = new FakeInteractionOptions(options?.options, options?.subCommand)
+	}
+
+	public get channelId() { return this.channel.id }
+	public get guildId() { return this.guild.id };
+
+	private message: Message | null = null
+
+	public isChatInputCommand() {return true}
+	public isRepliable() {return true}
+	public inGuild() { return true }
+
+	public fetchReply(): Promise<Message<boolean>> {
+		if (!this.message) {
+			EmitError(new Error('Unable to fetch reply of interaction'))
+		}
+
+		return new Promise((resolve) => {resolve(this.message!)})
+	}
+
+	public async reply(replyContent: string | {content: string, ephemeral: boolean, embeds: EmbedBuilder[], components: any[]}): Promise<Message|boolean> {
+		const channel = this.guild.channels.cache.get(this.channel.id)
+		if (!channel || !(channel instanceof TextChannel)) return false
+
+		this.message = await channel.send(replyContent)
+		return this.message
+	}
+
+
+	public execute() {
+		this.client.emit('interactionCreate', this as unknown as Interaction)
+	}
+}
