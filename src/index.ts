a465375a19b592784196b6015ec239de53f30a46
CTN <info@ctn-originals.com>
2025-03-08 13:09:35 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,52 @@
-import 'dotenv/config';
 import { Client, Collection, WebhookClient } from 'discord.js';
+import 'dotenv/config';
 
-import { ConsoleInstance, Theme, ThemeOverride, defaultThemeProfile, defaultFilterKeys } from 'better-console-utilities';
+import { ConsoleInstance, defaultThemeProfile } from 'better-console-utilities';
 
-import { registertAllEvents } from './register/registerEvents';
-import { registerAllCommands } from './register/registerCommands';
 import { GeneralData } from './data';
+import { registerAllCommands } from './register/registerCommands';
+import { registertAllEvents } from './register/registerEvents';
 
-import * as deployScript from './deployCommands';
 import { Database } from './data/orm/connect';
+import * as deployScript from './deployCommands';
+import { EmitError } from './events';
 
 //? Set the default theme profile to my preferences
 defaultThemeProfile.overrides.push(...[]);
-defaultFilterKeys.push(...((GeneralData.logging.streamSafe) ? ['token'] : []));
 
 export const cons = new ConsoleInstance();
 
 export const client: Client = new Client({
 	intents: [
 		'Guilds',
-		'GuildMessages',
-		'GuildMembers',
-		'MessageContent'
 	]
 });
 export const logWebhook = new WebhookClient({id: process.env.LOG_WEBHOOK_ID!, token: process.env.LOG_WEBHOOK_TOKEN!});
 export const testWebhook = new WebhookClient({id: process.env.TEST_WEBHOOK_ID!, token: process.env.TEST_WEBHOOK_TOKEN!});
 
-async function Awake() {
+async function Awake(): Promise<void> {
+	//- Check if more then one flag is true
+	if (
+		GeneralData.production && (GeneralData.beta || GeneralData.development) ||
+		GeneralData.beta && (GeneralData.development || GeneralData.production) ||
+		GeneralData.development && (GeneralData.production || GeneralData.beta)
+	) {
+		EmitError(new Error([
+			'More then one startup flags are set to true, these flags need to be exclusive',
+			`production (${GeneralData.production}), beta (${GeneralData.beta}), development (${GeneralData.development})`,
+		].join('\n')));
+		throw 'Start-up flags are non-exclusive';
+	}
+
 	client.commands = new Collection();
-	client.contextMenus = new Collection();
 	client.buttons = new Collection();
 	client.selectMenus = new Collection();
 	
-	registertAllEvents(client, 'events');
-	registerAllCommands(client, 'commands');
+	await registertAllEvents(client, 'events');
+	await registerAllCommands(client, 'commands');
 
 	if (process.argv.includes('--deploy')) {
 		cons.log(process.argv);
-		// const deployScript = require('./deployCommands.ts');
 		await deployScript.doDeployCommands(client).then(() => {
 			process.exit(0);
 		});
@@ -48,7 +56,7 @@ async function Awake() {
 	}
 }
 
-async function Start() {
+async function Start(): Promise<void> {
 	const db = new Database();
 	await db.connect();
 	
